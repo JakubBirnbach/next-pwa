ad3187b04e61d89ffda721a15ceee7615ef6970a
Jakub <jakub.birnbach@tsh.io>
2021-03-24 10:07:33 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
     dest: 'public',
     runtimeCaching,
   },
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,29 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
+        />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
         <title>Next.js PWA Example</title>
 
@@ -33,5 +45,5 @@ export default function MyApp({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
